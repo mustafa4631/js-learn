95d34658e38c1c8635eec1c46a8a60863b2d94c1
Mustafa Gökpınar <mustafagokpinar4646@gmail.com>
2026-02-07 20:05:59 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+//   console.log(typeof"mustafa")
+//   console.log(typeof 5)
+//   console.log(typeof true)
+//   console.log(typeof null)
+
+//let first_Name = "Mustafa",
+ //   surname = "Gökpınar",
+   // age = 3
+
+//console.log("merhaba benim adım",first_Name,"soyadım",surname,"ve yaşım",age)
+//console.log(first_Name[0])
+
+ //   let sayi = 5
+  //   let sayi1 = 5
+
+    // console.log(sayi > sayi1)
+
+ //console.log(sıra_no[2])
+const PI =Math.PI
+
+ let mal = {
+    firstname:"mustafa",
+    surname:"gökpınar",
+    age:3,
+    country:"türkiye",
+    city:"Kahramanmaraş"
+
+ }
+ console.log(mal)
+console.log(Math.ceil(8.8))
+console.log(PI)
